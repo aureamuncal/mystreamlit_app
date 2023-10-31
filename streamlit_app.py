3b6dc51e266a07c028ba653627c8e3b6b987668f
aureamuncal <149447849+aureamuncal@users.noreply.github.com>
2023-10-31 15:19:38 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 
+
 streamlit.header('Breakfast Favorite')
 streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach and Rocket Smoothie ')
